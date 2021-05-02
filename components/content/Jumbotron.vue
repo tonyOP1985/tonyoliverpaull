b4a5adb5f6829ae76c644d276cb1fb78f6e32b05
Tony Oliver-Paull <toliverpaull@gmail.com>
2021-05-02 17:12:12 -0400

    Added jumbotron to index/about page.

---
+++ components/content/Jumbotron.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <slot name="content" />
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+section {
+  width: 100%;
+  height: 210px;
+  background-color: rgba(72, 206, 181, 0.8);
+  color: #fff;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,198 @@
 <template>
-  <div class="container">
-    <h1>More stuff to come!!!</h1>
+  <div>
+    <Jumbotron>
+      <template #content>
+        <div class="body-container">
+          <header class="header-main">
+            <div class="text-box">
+              <div class="heading-primary">
+                <span class="heading-primary-main">Tony Oliver-Paull &mdash;</span>
+                <span class="heading-primary-sub">Full-Stack Web Developer</span>
+              </div>
+            </div>
+            <div class="link-box">
+              <div class="links">
+                <a href="https://github.com/tonyOP1985" target="_blank" class="btn icon github">
+                  <i class="fa fa-github fa-lg" aria-hidden="true" />
+                </a>
+                <a href="https://www.linkedin.com/in/tony-oliverpaull-62b21434/" target="_blank" class="btn icon">
+                  <i class="fa fa-linkedin fa-lg" aria-hidden="true" />
+                </a>
+                <router-link to="/contact" class="btn icon">
+                  <i class="fa fa-envelope" aria-hidden="true" />
+                </router-link>
+                <a href="../../static/Anthony_Oliver-Paull_Resume_2018.pdf" target="_blank" class="btn resume">résumé</a>
+              </div>
+            </div>
+          </header>
+        </div>
+      </template>
+    </Jumbotron>
   </div>
 </template>
 
 <script>
-export default {}
+import Jumbotron from '@/components/content/Jumbotron'
+
+export default {
+  components: {
+    Jumbotron
+  }
+}
 </script>
 
-<style>
-.container {
+<style lang="scss" scoped>
+.body-container {
+  width: 900px;
+  height: 100%;
   margin: 0 auto;
-  min-height: 100vh;
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: space-between;
+}
+
+.header-main {
+  width: 100%;
+}
+
+.text-box {
+  width: 80%;
+  margin: 0 auto;
+}
+
+.text-box,
+.link-box {
   text-align: center;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.heading-primary {
+  width: 100%;
+  font-size: 1.7rem;
+  font-weight: 400;
+  display: flex;
+  flex-direction: column;
+  transition: all 0.2s ease;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.link-box {
+  width: 100%;
+  margin: 20px auto 0 auto;
 }
 
 .links {
-  padding-top: 15px;
+  width: 255px;
+  height: 42px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  margin: 0 auto;
+}
+
+.icon {
+  display: block;
+  position: relative;
+  text-align: left;
+  width: 41px;
+  height: 41px;
+
+  /* background-color:#0291b4; */
+  border-radius: 100%;
+
+  /* color: #fff; */
+  color: rgba(238, 237, 237, 0.8);
+  transition: all 0.2s ease;
+}
+
+a {
+  text-decoration: none;
+}
+
+.icon i {
+  width: 41px;
+  height: 41px;
+  padding-top: 31%;
+}
+
+.resume {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 115px;
+  height: 41px;
+  padding-top: 2px;
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: 2px;
+  border: 1px solid rgba(238, 237, 237, 0.8);
+  border-radius: 5px;
+  color: rgba(238, 237, 237, 0.8);
+  text-transform: uppercase;
+  transition: all 0.2s ease;
+}
+
+.btn:hover {
+  color: #fff;
+  border-color: #fff;
+}
+
+@media (max-width: 1200px) {
+  .body-container {
+    width: 80%;
+  }
+
+  .greeting {
+    width: 100%;
+    font-size: 8rem;
+  }
+
+  .text-box {
+    margin: 0 auto;
+  }
+}
+
+@media (max-width: 1000px) {
+  .text-box {
+    margin: 0 auto;
+  }
+}
+
+@media (max-width: 770px) {
+  .body-container {
+    width: 100%;
+  }
+
+  .greeting {
+    font-size: 6rem;
+    margin-left: 8px;
+  }
+
+  .text-box {
+    width: 100%;
+  }
+
+  .link-box {
+    width: 100%;
+  }
+}
+
+@media (max-width: 400px) {
+  .heading-primary {
+    font-size: 1.2rem;
+  }
+}
+
+@media (max-width: 360px) {
+  .greeting {
+    font-size: 4rem;
+  }
+
+  .icon {
+    margin-right: 5px;
+  }
+
+  .links {
+    width: 250px;
+    margin: 0 auto;
+  }
 }
 </style>
